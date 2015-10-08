819d9826804751c22af22b7e52708c75b64439ec
flipps007 <lisaburr@gmx.de>
2015-10-08 09:17:31 +0200

    added yesterday

---
+++ javascript.js
@@ -0,0 +1,11 @@
+function previousDay() {
+    document.getElementById("temperature").innerHTML = "12°C";
+    document.getElementById("sunny").src = "img/rain.png";
+    document.getElementById("summary").innerHTML = "regen";
+}
+
+function nextDay() {
+    document.getElementById("temperature").innerHTML = "30°C";
+    document.getElementById("sunny").src = "img/rain.png";
+    document.getElementById("summary").innerHTML = "regen";
+}
