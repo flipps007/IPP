44da71862b9b3a2dc8aaea48753e476993cf4231
flipps007 <lisaburr@gmx.de>
2015-10-08 13:58:10 +0200

    8.10.

--- javascript.js
+++ javascript.js
@@ -1,11 +1,46 @@
+var d = new Date();
+var weather = {
+    time: 1444298150918,
+    summary: "sonne",
+    icon: "img/clear-day.png",
+    humidity: 3,
+    windspeed: 10,
+    temperature: 23
+}
+
+function getCurrentData() {
+    document.getElementById("actualdate").innerHTML = d.toLocaleDateString();
+    document.getElementById("temperature").innerHTML = weather.temperature;
+    document.getElementById("sunny").innerHTML = weather.icon;
+    document.getElementById("summary").innerHTML = weather.summary;
+    document.getElementById("humidity").innerHTML = weather.humidity + "%";
+    document.getElementById("windspeed").innerHTML = weather.windspeed;
+}
+
 function previousDay() {
     document.getElementById("temperature").innerHTML = "12°C";
     document.getElementById("sunny").src = "img/rain.png";
     document.getElementById("summary").innerHTML = "regen";
+    d.setDate(d.getDate() - 1);
+    document.getElementById("actualdate").innerHTML = d.toLocaleDateString();
 }
 
 function nextDay() {
     document.getElementById("temperature").innerHTML = "30°C";
     document.getElementById("sunny").src = "img/rain.png";
     document.getElementById("summary").innerHTML = "regen";
+    d.setDate(d.getDate() + 1);
+    document.getElementById("actualdate").innerHTML = d.toLocaleDateString();
+}
+
+function convertWindspeed(speed, format) {
+    return speed * 3.6;
+}
+
+function sidebar() {
+    document.getElementById("sidebar").style.display = "block";
+}
+
+function sidebarClose() {
+    document.getElementById("sidebar").style.display = "none";
 }
